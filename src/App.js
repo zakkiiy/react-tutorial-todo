06f651380db1107f2ad8f90ceb83e1bd5e9212de
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2023-10-21 15:00:30 +0900

    todoコンポーネントおよびpropsの渡し方について

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import TodoList from "./TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, name:"Todo1", completed: false}
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
     </div>
   );
 }
