0faf27ecbc755adc872d031ad89f6c8a65b4a657
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2023-10-21 15:51:57 +0900

    タスクの追加とチェックボックスの判定

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import TodoList from "./TodoList";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [todos, setTodos] = useState([
     {id: 1, name:"Todo1", completed: false}
   ]);
 
+  //refを指定した要素を取得できる
+  const todoNameRef = useRef();
+
+  const handleAddTodo = () => {
+    //タスクを追加する関数
+    const name = todoNameRef.current.value;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
+    });
+    todoNameRef.current.value = null;
+  };
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
+
   return (
     <div>
-      <TodoList todos={todos} />
-      <input type="text" />
-      <button>タスクを追加</button>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input type="text" ref={todoNameRef} />
+      <button onClick={handleAddTodo}>タスクを追加</button>
       <button>完了したタスクの削除</button>
       <div>残りのタスク:0</div>
     </div>
